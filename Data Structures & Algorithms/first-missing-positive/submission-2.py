d1ad927209201e4a10bfc923252b1cecb8bdc723
deniz2105 <81939026+deniz2105@users.noreply.github.com>
2026-06-28 00:36:02 -0400

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        rangeD = {}
+        ptr = 2 ** 31
+        inBetweenVal = float('inf')
+        rangeD[ptr] = -1
+        for i in range(len(nums)):
+            if nums[i] == inBetweenVal:
+                inBetweenVal = float('inf')
+            if nums[i] < 1:
+                continue
+            elif nums[i] < ptr:
+                if ptr - nums[i] != 1 and inBetweenVal > nums[i]+1:
+                    inBetweenVal = nums[i]+1
+                val = rangeD.pop(ptr)
+                ptr = nums[i]
+                rangeD[ptr] = val
+            elif rangeD[ptr] < nums[i]:
+                if rangeD[ptr] != -1 and nums[i]-rangeD[ptr] != 1 and inBetweenVal > nums[i]-1:
+                    inBetweenVal = rangeD[ptr]+1
+                rangeD[ptr] = nums[i]
+
+        if ptr > 1:
+            return 1
+        elif inBetweenVal != float('inf'):
+            return inBetweenVal
+        else: 
+            return rangeD[ptr] +1
+
+            
+                
+        
+
+
+        
+            
+
