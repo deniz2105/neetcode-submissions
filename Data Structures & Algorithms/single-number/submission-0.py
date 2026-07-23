f5999cc02e9ffdd8826e8e882e450e892f8ff5bc
deniz2105 <81939026+deniz2105@users.noreply.github.com>
2026-07-22 22:13:12 -0400

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        curr = nums[0]
+        for i in range(1,len(nums)):
+            curr = curr ^ nums[i]
+        return curr
+            
